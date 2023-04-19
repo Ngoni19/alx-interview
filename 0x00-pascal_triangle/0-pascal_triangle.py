0b15d4b42bd36ed28634745944b2187736e94e34
Ngoni19 <ngoni19@live.com>
2023-04-19 18:21:30 +0000

    add function - pascal

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,36 +1,29 @@
 #!/usr/bin/python3
-"""
+'''
 A module containing an implementation of Pascal's Triangle function.
-"""
-
-from functools import reduce
+'''
 
 
-def combination(n, r):
-    """
-    Compute the combination of two variables.
-    """
+def _combination(n, r):
+    '''Compute the combination of two variables'''
+    from functools import reduce
+
     def factorial(n):
-        """
-        Find the factorial of an int
-        """
-        fac = 1
-        for x in range(1, n+1):
-            fac *= x
-        return fac
+        ''' Compute the combination of two variables.'''
+        facto = 1
+        for z in range(1, n+1):
+            facto *= z
+        return facto
     return factorial(n) // (factorial(n - r) * factorial(r))
 
 
 def pascal_triangle(n):
-    """
-    Generates the Pascal's Triangle for a given power n, excluding (x+y)^n.
-    """
-    assert type(n) == int, "n must be an integer!"
+    '''Generates the pascal triangle for a power, excluding (z+y)^power'''
+    assert type(n) == int "n must be an integer!"
     if n <= 0:
         return []
 
-    triangle = [[1]] + [
-        [combination(x, y) for y in range(x+1)]
-        for x in range(1, n)
+    return [[1]] + [
+        [_combination(z, y) for y in range(z+1)]
+        for z in range(1, n)
     ]
-    return triangle
