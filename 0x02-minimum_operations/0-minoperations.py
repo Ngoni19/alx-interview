5c0e3f2914ec4d39b105e0c26a51aa34cbc62560
Ngonidzashe Brandon Towindo <ngoni@live.com>
2023-05-04 19:09:36 +0200

    Update 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -7,13 +7,13 @@ def minOperations(n):
     '''Method: computes the fewest number of operations needed to result
     in exactly (n) H characters.
     '''
-    if not isinstance(n, int) or n < 1:
+    if n < 1:
         return 0
-    # use memoization to avoid recomputation
-    memo = {0: 0, 1: 0}
+    # use memoization to avoid recomputation    
+    memo = [0, 0] + [float('inf')] * (n - 1)
     for k in range(2, n + 1):
-        memo[k] = float('inf')
-        for j in range(1, k):
+        for j in range(2, k + 1):
             if k % j == 0:
-                memo[k] = min(memo[k], memo[j] + k // j)
-    return memo[n]
+                memo[k] = min(memo[k], memo[j] + (k // j))
+                break
+    return memo[n] if memo[n] != float('inf') else 0
