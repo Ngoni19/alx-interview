b3f613ace6a1cc39b121ec84fb171db77df9cb02
Ngonidzashe Brandon Towindo <ngoni@live.com>
2023-05-21 23:32:30 +0200

    Update 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 '''
 A module that contains a function that determines if a given data set
-represents a valid UTF-8 encoding.
+repre a valid UTF-8 encoding.
 '''
 
 
@@ -18,18 +18,18 @@ def validUTF8(data):
     '''
     byte_count = 0
 
-    for i in data:
+    for x in data:
         if byte_count == 0:
-            if i >> 5 == 0b110 or i >> 5 == 0b1110:
+            if x >> 5 == 0b110 or x >> 5 == 0b1110:
                 byte_count = 1
-            elif i >> 4 == 0b1110:
+            elif x >> 4 == 0b1110:
                 byte_count = 2
-            elif i >> 3 == 0b11110:
+            elif x >> 3 == 0b11110:
                 byte_count = 3
-            elif i >> 7 == 0b1:
+            elif x >> 7 == 0b1:
                 return False
         else:
-            if i >> 6 != 0b10:
+            if x >> 6 != 0b10:
                 return False
             byte_count -= 1
     return byte_count == 0
