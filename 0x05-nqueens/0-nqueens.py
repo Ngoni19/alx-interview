e190959e54bc6bf67400a46df10511d7105cf657
Ngonidzashe Brandon Towindo <ngoni@live.com>
2023-05-30 21:49:10 +0200

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -45,9 +45,9 @@ if __name__ == '__main__':
         print("Usage: nqueens N")
         sys.exit(1)
 
+    n = int(sys.argv[1])
+    
     try:
-        n = int(sys.argv[1])
-
         if n < 4:
             print('N must be at least 4')
             sys.exit(1)
