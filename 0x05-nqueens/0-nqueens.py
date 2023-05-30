d5d0c416b9f6f4d800d265947d7a9c1cec5029bb
Ngonidzashe Brandon Towindo <ngoni@live.com>
2023-05-30 21:01:34 +0200

    Create 0-nqueens.py

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+'''Script -> N Queens Challenge'''
+
+import sys
+
+
+def is_safe(placed_queens, row, col):
+    for r, c in placed_queens:
+        if col == c or row - col == r - c or row + col == r + c:
+            return False
+    return True
+
+
+def solve_n_queens(n):
+    if n < 4:
+        return []
+
+    solutions = []
+    placed_queens = []
+
+    def backtrack(row):
+        if row == n:
+            solutions.append(placed_queens[:])
+            return
+        for col in range(n):
+            if is_safe(placed_queens, row, col):
+                placed_queens.append((row, col))
+                backtrack(row + 1)
+                placed_queens.pop()
+
+    backtrack(0)
+    return solutions
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print('N must be a number')
+        exit(1)
+
+    solutions = solve_n_queens(n)
+
+    for idx, val in enumerate(solutions):
+        if idx == len(solutions) - 1:
+            print(val, end='')
+        else:
+            print(val)
