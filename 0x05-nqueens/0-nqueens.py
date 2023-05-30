f1e9801fb3d0d89aed440a85960de6bf8619bdbf
Ngonidzashe Brandon Towindo <ngoni@live.com>
2023-05-30 21:37:31 +0200

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -52,7 +52,6 @@ if __name__ == '__main__':
         if n < 4:
         print('N must be at least 4')
         sys.exit(1)
-        
     except ValueError:
         print('N must be a number')
         exit(1)
