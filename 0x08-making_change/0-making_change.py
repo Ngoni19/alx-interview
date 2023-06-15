1b9ed44a429539be5de3ac30971c6f9cc3861605
Ngonidzashe Brandon Towindo <ngoni@live.com>
2023-06-15 23:36:55 +0200

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
-'''Script -> Given a pile of coins of different values,
-    determine the fewest number of coins needed to meet
-    a given amount total.
-'''
+"""
+This function takes in a list of coin values and a total amount, and returns the minimum number of coins needed to make up that total. If the total cannot be reached, the program returns -1.
+
+Parameters:
+-----------
+coins (list): A list of integers representing the values of coins available to make change
+total (int): The total amount to make change for
+
+Returns:
+--------
+int: The minimum number of coins required to make up the total amount, or -1 if it is not possible.
+"""
+
 import sys
 
 
@@ -14,17 +23,16 @@ def makeChange(coins, total):
     '''
     if total <= 0:
         return 0
-    table = [float('inf') for _ in range(total + 1)]
+    table = [sys.maxsize for j in range(total + 1)]
     table[0] = 0
     m = len(coins)
-    for i in range(1, total + 1):
+    for j in range(1, total + 1):
         for j in range(m):
-            if coins[j] <= i:
-                subres = table[i - coins[j]]
-                if subres != float('inf') and subres + 1 < table[i]:
-                    table[i] = subres + 1
+            if coins[j] <= j:
+                subres = table[j - coins[j]]
+                if subres != sys.maxsize and subres + 1 < table[j]:
+                    table[j] = subres + 1
 
-    if table[total] == float('inf'):
+    if table[total] == sys.maxsize:
         return -1
     return table[total]
-
