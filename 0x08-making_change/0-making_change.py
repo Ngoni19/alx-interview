425a3af6da85d4d37cc031e32ab3484f00749059
Ngonidzashe Brandon Towindo <ngoni@live.com>
2023-06-15 23:50:21 +0200

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,17 +1,8 @@
 #!/usr/bin/python3
-"""
-This function takes in a list of coin values and a total amount, and returns the minimum number of coins needed to make up that total. If the total cannot be reached, the program returns -1.
-
-Parameters:
------------
-coins (list): A list of integers representing the values of coins available to make change
-total (int): The total amount to make change for
-
-Returns:
---------
-int: The minimum number of coins required to make up the total amount, or -1 if it is not possible.
-"""
-
+'''Scipt-> Given a pile of coins of different values,
+    determine the fewest number of coins needed to meet
+    a given amount total.
+'''
 import sys
 
 
@@ -23,15 +14,15 @@ def makeChange(coins, total):
     '''
     if total <= 0:
         return 0
-    table = [sys.maxsize for j in range(total + 1)]
+    table = [sys.maxsize for k in range(total + 1)]
     table[0] = 0
     m = len(coins)
-    for j in range(1, total + 1):
+    for k in range(1, total + 1):
         for j in range(m):
-            if coins[j] <= j:
-                subres = table[j - coins[j]]
-                if subres != sys.maxsize and subres + 1 < table[j]:
-                    table[j] = subres + 1
+            if coins[j] <= k:
+                subres = table[k - coins[j]]
+                if subres != sys.maxsize and subres + 1 < table[k]:
+                    table[k] = subres + 1
 
     if table[total] == sys.maxsize:
         return -1
