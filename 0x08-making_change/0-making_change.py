f6a53d9ff7aa0e6354fd205abf3a324a11a5ccec
Ngonidzashe Brandon Towindo <ngoni@live.com>
2023-06-16 00:22:46 +0200

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,6 +7,11 @@ import sys
 
 
 def makeChange(coins, total):
+    '''
+    Return: fewest number of coins needed to meet total
+    If total is 0 or less, return 0
+    If total cannot be met by any number of coins you have, return -1
+    '''
     if total <= 0:
         return 0
     table = [sys.maxsize for _ in range(total + 1)]
